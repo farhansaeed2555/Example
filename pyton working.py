ae584803a147d7c485255d823eaa73a75b9f8380
farhansaeed2555 <157400807+farhansaeed2555@users.noreply.github.com>
2024-01-24 22:46:22 +0500

    Add files via upload

---
+++ pyton working.py
@@ -0,0 +1,71 @@
+num1 = 5
+print(num1, 'is of type', type(num1))
+num2 = 2.0
+print(num2, 'is of type', type(num2))
+num3 = 1+2j
+print(num3, 'is of type', type(num3))
+
+# logical AND
+print(True and False) #True
+print(True and False) #False
+# logical OR
+print(True or False) #True
+# logocal NOt
+print(not True) #False
+
+a = 5
+b = 2
+# equal to operator
+print('a == b =', a == b)
+# not equal to operator
+print('a != b =', a != b)
+# greather equal to operator
+print('a > b =', a > b)
+# less equal to operator
+print('a < b =', a < b)
+# greatherthan or equal to operator
+print('a >= b =', a >= b)
+# lessthan or equal to operator
+print('a <= b =', a <= b)
+
+# assign 10 to a
+a = 10
+# assign 5 to b
+b = 15
+# assign the sum of a and b to a
+a += b
+print (a)
+
+a = 4
+b = 5
+# addition
+print ('sum: ', a + b)
+# subtraction
+print ('subtraction: ', a - b)
+#multiplication
+print ('multiplication: ', a * b)
+# division
+print ('division: ', a / b)
+# floor division
+print ('floor division: ', a // b)
+
+# Get user input 
+name = input("what is your name?")
+num = input("what is your favorite number?")
+print("")
+# Uses user input to print out infomation
+print("hello" + name + "!")
+print(num + "/ that's my favorite number tool!")
+print("") 
+
+# using input() to take user input
+num = input('Enter a number')
+print('You Entered:', num)
+print('Data type of num:', type(num))
+
+# using input() to take user input
+num = input('Enter a number')
+print('You Entered:', num)
+print('Data type of num:', type(num))
+
+
